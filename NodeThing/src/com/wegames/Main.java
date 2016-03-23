4dc6dd9d6cbb5e87d54d8fa984254de2d4e65be8
RomanResistance <romanresistance@gmail.com>
2016-03-23 11:01:08 -0600

    Added Method to get Nearby Nodes.
    Method getNearbyNodes requires the input of a node, also sorts the
    nodes.

--- NodeThing/src/com/wegames/Main.java
+++ NodeThing/src/com/wegames/Main.java
@@ -22,45 +22,47 @@ import com.wegames.level.Resource;
  *
  */
 public class Main extends Canvas implements Runnable {
-	
+
 	Thread thread;
 	boolean running = false;
-	
-	int width = 1080, height = width/16*9;
+
+	int width = 1080, height = width / 16 * 9;
 	JFrame frame;
 	Screen screen;
 	BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
-	
+	int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
 	/**
 	 * 
 	 */
 	public Main() {
-		frame=new JFrame();
+		frame = new JFrame();
 		Dimension dim = new Dimension(width, height);
 		frame.setMinimumSize(dim);
 		frame.setMaximumSize(dim);
 		frame.setPreferredSize(dim);
-		
+
 		screen = new Screen(width, height);
 	}
-	
+
 	public synchronized void start() {
-		running=true;
+		running = true;
 		thread = new Thread(this);
 		thread.start();
 	}
-	
+
 	public synchronized void stop() {
-		running=false;
+		running = false;
 		try {
 			thread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
@@ -71,23 +73,23 @@ public class Main extends Canvas implements Runnable {
 		double secondsPerTick = 1 / 60.0;
 		int tickCount = 0;
 		boolean ticked = false;
-		while(running) {
+		while (running) {
 			long currentTime = System.nanoTime();
 			long passedTime = currentTime - previousTime;
 			previousTime = currentTime;
 			unprocesedSeconds += passedTime / 1000000000.0;
-			while(unprocesedSeconds > secondsPerTick) {
+			while (unprocesedSeconds > secondsPerTick) {
 				tick();
 				unprocesedSeconds -= secondsPerTick;
 				ticked = true;
 				tickCount++;
-				if(tickCount % 60 == 0) {
+				if (tickCount % 60 == 0) {
 					System.out.println("Frames Per Second: " + frames + " | Ticks: " + tickCount);
 					previousTime += 1000;
 					frames = 0;
 				}
 			}
-			if(ticked) {
+			if (ticked) {
 				render();
 				frames++;
 			}
@@ -96,30 +98,31 @@ public class Main extends Canvas implements Runnable {
 	}
 
 	private void tick() {
-		
+
 	}
 
 	private void render() {
 		BufferStrategy bs = getBufferStrategy();
-		if(bs==null) {
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-		
+
 		screen.drawCircle(100, 100, 100, 0xffffffff);
 		screen.drawLine(100, 100, 500, 500, 0x99999999);
-		
+
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = screen.pixels[i];
 		}
-		
+
 		Graphics g = bs.getDrawGraphics();
-		
-		g.drawImage(image, 0, 0, frame.getWidth()*3, frame.getHeight()*3, null);
-		
+
+		g.drawImage(image, 0, 0, frame.getWidth() * 3, frame.getHeight() * 3, null);
+
 		g.dispose();
 		bs.show();
 	}
+
 	/**
 	 * @param args
 	 */
--- NodeThing/src/com/wegames/level/Node.java
+++ NodeThing/src/com/wegames/level/Node.java
@@ -4,8 +4,10 @@
 package com.wegames.level;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -16,6 +18,10 @@ import java.util.Random;
 public class Node {
 	public int x, y;
 	
+	public static List<Node> nodes = new ArrayList<Node>();
+	
+	int maxDistance=400;
+	
 	Map<Integer, Integer> rawSupply = new HashMap<Integer, Integer>();
 	Map<Integer, Integer> productionHarvesting = new HashMap<Integer, Integer>();
 	
@@ -24,6 +30,8 @@ public class Node {
 	Map<Integer, Integer> excess = new HashMap<Integer, Integer>();
 	Map<Integer, Integer> productionExcess = new HashMap<Integer, Integer>();
 	
+	List<Node> nearbyNodes=new ArrayList<Node>();
+	
 	public double getDistance(Node node) {
 		int dx = node.x-x;
 		int dy = node.y-y;
@@ -31,7 +39,24 @@ public class Node {
 		return Math.sqrt(Math.pow(dx, 2)+Math.pow(dy, 2));
 	}
 	
-	public void generateResources() {
+	public static List<Node> getNearbyNodes(Node target) {
+		List<Node> result = new ArrayList<Node>();
+		for (int i = 0; i < nodes.size(); i++) {
+			if(target.getDistance(nodes.get(i))<=target.maxDistance) result.add(nodes.get(i));
+		}
+		
+		result.sort(new Comparator<Node>() {
+			public int compare(Node o1, Node o2) {
+				if(target.getDistance(o1)<target.getDistance(o2)) return 1;
+				else if(target.getDistance(o1)>target.getDistance(o2)) return-1;
+				else return 0;
+			}
+		});
+		
+		return result;
+	}
+	
+	private void generateResources() {
 		Random random = new Random();
 		int standardSize = random.nextInt(100)/8;
 		for (int i = 0; i < Resource.resources.size(); i++) {
@@ -54,8 +79,17 @@ public class Node {
 			}
 		}
 	}
-	
+	int toUpdate=0;
 	public void tick() {
-		
+		if(toUpdate<1800) {
+			toUpdate++;
+		} else {
+			toUpdate=0;
+			calculateResources();
+		}
+	}
+	public Node() {
+		nodes.add(this);
+		generateResources();
 	}
 }
